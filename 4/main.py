7b97bebb94abd8e0970ebf5af5a991b45502ee65
Valentin Boettcher <hiro@protagon.space>
2019-05-06 22:12:01 +0200

    fix inexact timing, add 4-periodic orbit as preset

--- 4/main.py
+++ 4/main.py
@@ -25,7 +25,7 @@ def set_up_plots():
 
     for ax in ax_tr, ax_st:
         ax.set_xlabel(r'$x$')
-        ax.set_xlabel(r'$x$')
+        ax.set_ylabel(r'$p$')
 
         # set fixed aspect ratio
         ax.set_aspect('equal')
@@ -54,19 +54,14 @@ def rhs(y, t, A, B, omega):
     x, p = y
     return np.array([p, -4*x**3 + 2*x - A - B*np.sin(omega*t)])
 
-def add_trajectory(ax_tr, ax_st, periods,
-                   steps_per_period, args, event):
-
-    # only react if not zooming
-    mode = event.canvas.toolbar.mode
-    if not (event.button == 1 and event.inaxes == ax_tr or event.inaxes ==
-            ax_st and mode == ''):
-        return
-
+def add_trajectory(ax_tr, ax_st, periods, steps_per_period,
+                   initial_conditions, args):
     # solve the DE
     _, _, omega = args
-    t_range = np.linspace(0, periods*2*np.pi/omega, steps_per_period*periods)
-    trajectory = odeint(rhs, (event.xdata, event.ydata), t_range, args=args)
+    t_range = np.arange(0, steps_per_period*periods)/steps_per_period \
+        * 2 * np.pi / omega
+    trajectory = odeint(rhs, initial_conditions, t_range,
+                        args=args)
 
     # unpack the solution
     x, p = trajectory.T
@@ -78,6 +73,25 @@ def add_trajectory(ax_tr, ax_st, periods,
     ax_st.plot(x[::steps_per_period], p[::steps_per_period], linestyle='none',
                marker='o', markersize=0.8)
 
+def handle_mouse_click(ax_tr, ax_st, periods,
+                   steps_per_period, args, event):
+
+    # only react if not zooming
+    mode = event.canvas.toolbar.mode
+    if event.button != 1 or mode != '' or (event.inaxes != ax_tr and
+                                       event.inaxes != ax_st):
+        return
+
+
+    add_trajectory(ax_tr, ax_st, periods,
+                   steps_per_period, (event.xdata, event.ydata), args)
+    event.canvas.draw()
+
+def handle_key_press(ax_tr, ax_st, event):
+    if event.key == 'c':
+        ax_tr.lines = []
+        ax_st.lines = []
+
     event.canvas.draw()
 
 def main():
@@ -88,19 +102,28 @@ def main():
     print(__doc__)
 
     # System Constants
-    A = 0 # 0.2
-    B = 0 # 0.1
+    A = 0.2
+    B = 0.1
     omega = 1
 
     # Solver Parameters
-    periods = 200  # how many drive periods to solve for
-    steps_per_period = 20
+    periods = 2000  # how many drive periods to solve for
+    steps_per_period = 1
 
+    # set up figures and listeners
     fig, (ax_tr, ax_st) = set_up_plots()
-    on_click = functools.partial(add_trajectory, ax_tr, ax_st, periods,
+    on_click = functools.partial(handle_mouse_click, ax_tr, ax_st, periods,
                                  steps_per_period, (A, B, omega))
 
     fig.canvas.mpl_connect('button_press_event', on_click)
+
+    # we do it in here, because it is minor
+    on_key_press = functools.partial(handle_key_press, ax_tr, ax_st)
+    fig.canvas.mpl_connect('key_press_event',
+                           on_key_press)
+
+    add_trajectory(ax_tr, ax_st, periods, steps_per_period, (0.231704,
+                                                             -0.349683), (A, B, omega))
     plt.show()
 
 if __name__ == '__main__':
